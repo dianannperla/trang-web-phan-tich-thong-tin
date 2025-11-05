017f392cf3b60570018a5d00bdb0f11ff50a7f7d
dianannperla <waafirmelyza@hotmail.com>
2025-11-05 11:40:44 +0000

    Update log message for better clarity

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('The application has successfully loaded!');
+    console.log('The application has loaded successfully!');
 });
